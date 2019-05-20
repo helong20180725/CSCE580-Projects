832afccea594b56a9199a2386c58e905a3a6681e
Long He <longhe@Longs-MBP.lan1>
2019-05-19 23:16:32 -0400

    machine learning, unfinished

---
+++ machinelearning/helloTest.py
@@ -0,0 +1,51 @@
+#note
+#1. Strings
+"""
+print("helloworld")
+
+
+capital="HI, YOU ARE CAPITAL LETTERS"
+print(capital.lower())
+print(capital.isupper())
+print(capital[4])
+
+
+print(capital.index("T"))
+print(capital.replace("ARE","is"))
+print("\"")
+"""
+#2. numbers
+"""
+a =10
+b = 3
+c = -19
+d = 4.23
+print(10%3)
+
+#error print("the number is "+a)
+print("the number is "+str(a))
+print(pow(a,b))
+print(abs(c))
+print(max(a,b,c))   # min()
+print(round(d))
+"""
+"""
+from math import *
+print(ceil(3.1))
+print(sqrt(4))
+"""
+
+#3.input
+"""
+a = input("Please input something: ")
+print(a+" right!")
+"""
+#4. build a basic calculator
+"""
+numberOne= int(input("The first number: "))
+numberTwo= int(input("The second number: "))
+result = float(numberOne) + numberTwo
+
+print(result)
+"""
+#5. mad libs
